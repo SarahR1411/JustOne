5b7a96958196bb6ad025a76c92d31732150d2c00
Emma <119598417+ritafari@users.noreply.github.com>
2025-01-31 17:18:34 +0100

    Pseudocode for Player and Validation

--- src/players.js
+++ src/players.js
@@ -1,1 +1,36 @@
 // Player management
+
+// ACTIVE PLAYER
+// STEP 1:
+    // choose one card out of 13
+    // choose a number between 1 and 5 (user input)
+// STEP 2:
+    // Collect the four clues
+    // Try to guess mystery word (user input)
+
+
+
+// OTHER PLAYERS
+// Players : p1, p2, p3, p4
+// Display the Mystery Word (the one chosen by active player in the beginning, without him knowing)
+// for players form 1 to 4:
+    // Prompt for user input so that player can input clue
+    // Erase the clue entered from previous player in terminal, so each player doesn't know what the other player wrote!!!
+
+// while validation == False (rules NOT followed): ==> Validation.js should display problem explicitly with player name that fucked up!!
+    // prompted user input again for player that fucked up, for new clue
+
+
+
+// ACTIVE PLAYER
+// Display all the clues together to the active player
+// STEP 3:
+    // If the guess is correct, the player wins => return WIN
+    // If the guess is incorrect, the player loses => return LOSE
+    // If the player wishes to pass => return PASS
+
+    
+
+// END OF ROUND
+// Switch Active Player: 
+    // The player that is on their left, becomes the Active Player 
--- src/validation.js
+++ src/validation.js
@@ -1,1 +1,30 @@
 // Clue validation
+
+// Collect clue from each player in the form of: {p1: "clue1", p2: "clue2", p3: "clue3", p4: "clue4"}
+
+// Validated = True
+// For each player from 1 to 4:
+    // If clue[i]==clue[j] (i!=j) 
+        // Validated = False
+        // problem = "Player [i], re-enter clue that isn't the same as others."
+
+    // If clue[i]==Mystery Word
+        // Validated = False
+        // problem = "Player [i], re-enter clue that isn't the same as the Mystery Word."
+
+    // If clue[i] is of same family as Mystery Word 
+        // Validated = False
+        // problem = "Player [i], re-enter clue that isn't of the same family as the Mystery Word."
+
+    // If clue[i] is a word that does not exist
+        // Validated = False
+        // problem = "Player [i], re-enter clue that is a word that exists."
+
+    // If clue[i] is phonetically the same as Mystery Word
+        // Validated = False
+        // problem = "Player [i], re-enter clue that isn't phonetically the same as the Mystery Word."
+
+// return Validated, problem
+
+
+
